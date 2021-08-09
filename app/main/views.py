5420861f0e61b8f79ca846f60b64dd55a830f7ff
marykamau2 <mary.kamau@student.moringaschool.com>
2021-08-09 11:16:06 +0300

    view root page function that returns the index page and its data

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,29 @@
-from ..requests import get_news
-from . import main
-from flask import render_template, request
-from ..models import Articles
 
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_news_source,get_articles
+from ..models import NewsSource
+
+#views
 @main.route('/')
 def index():
-    tesla_news = get_news('tesla')
-    bitcoin_news=get_news('bitcoin')
-    apple_news=get_news('apple')
-    title = 'Welcome to the news platform'
-    return render_template('index.html', bitcoin=bitcoin_news, tesla=tesla_news, apple=apple_news)
+	'''
+	view root page function that returns the index page and its data
+	'''
+	technology = get_news_source('technology')
+	business = get_news_source('business')
+	sports = get_news_source('sports')
+	entertainment = get_news_source('entertainment')
+	title = "News Room"
+
+	return render_template('index.html',title = title, technology = technology,business = business,sports = sports,entertainment = entertainment)
+
+@main.route('/news_articles/<id>')
+def articles(id):
+	'''
+	view articles  page function that returns the articles page and its data
+	'''
+	articles = get_articles(id)
+	title = f'News Room articles | {id}'
+
+	return render_template('news_articles.html',title= title,articles = articles)
